da9076a192f4f7651b86b6d188c4d3526a7c4346
Luis Almeida <luis.ega@hotmail.com>
2023-05-08 12:58:27 -0300

    display also fixed

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>QR Code Generator</h1>
       </header>
       <main className='App-main'>
         <StringInput />
         <QRComponent />
       </main>
       <footer className='App-footer'>
+        <a className="App-link" href="https://github.com/Ginosik">Ginosik</a>
       </footer>
     </div>
   );
--- src/Components/QRComponent/QRComponent.jsx
+++ src/Components/QRComponent/QRComponent.jsx
@@ -8,15 +8,48 @@ import { QRCodeSVG } from "qrcode.react";
 const QRComponent = () => {
   const link = useSelector((state) => state.qrcode.link);
 
+  const downloadQRCode = async () => {
+    const qrCodeWrapper = document.getElementById("qrCodeWrapper");
+    const qrCodeSvg = qrCodeWrapper.querySelector("svg");
+    if (!qrCodeSvg) return;
+  
+    const svgData = new XMLSerializer().serializeToString(qrCodeSvg);
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+    const img = new Image();
+    const svgBlob = new Blob([svgData], { type: "image/svg+xml;charset=utf-8" });
+    const imgUrl = URL.createObjectURL(svgBlob);
+  
+    img.onload = () => {
+      canvas.width = img.width;
+      canvas.height = img.height;
+      ctx.drawImage(img, 0, 0);
+  
+      const pngUrl = canvas.toDataURL("image/png");
+      const link = document.createElement("a");
+      link.href = pngUrl;
+      link.download = "qrcode.png";
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+      URL.revokeObjectURL(imgUrl);
+    };
+  
+    img.src = imgUrl;
+  };
+
   return (
-    <div className="qr-container">
+    <div className="qr-container" id="qrCodeWrapper">
       {link ? (
-        <QRCodeSVG
-          value={link}
-          size={300}
-          bgColor={"#ffffff"}
-          fgColor={"000000"}
-        />
+        <>
+          <QRCodeSVG
+            value={link}
+            size={300}
+            bgColor={"rgba(0, 0, 0, 0)"}
+            fgColor={"#000000"}
+          />
+          <button onClick={downloadQRCode}>Download QR Code</button>
+        </>
       ) : (
         <p>Please enter a link to generate a QR code.</p>
       )}
